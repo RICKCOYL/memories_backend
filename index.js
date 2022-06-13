60df7419260d049329da6864184a576ecbd6f21e
RICKCOYL <rickoburu@gmail.com>
2022-06-13 17:01:12 +0300

    initial

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-
 app.use('/posts', postRoutes);
 
 const CONNECTION_URL = `${dotenv.config().parsed.DB_MONGODB_URI}`; 
